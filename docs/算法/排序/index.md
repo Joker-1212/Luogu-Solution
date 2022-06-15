ee46a329e96c18cc3e5bd17f66a94bb4c99813de
Joker_1212 <joker_1212@163.com>
2022-06-15 15:36:47 +0800

    P1059 C++ AC

---
+++ docs/算法/排序/index.md
@@ -0,0 +1,3 @@
+# <font color=2949B4>排序</font>
+
+使用<font color=2949B4>排序</font>算法的题目的题解
---
+++ docs/题库/主题库/入门/P1059/Code.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    bool bucket[1001]{0};
+    int n, x, ans = 0;
+    scanf("%d", &n);
+    while (n--)
+    {
+        scanf("%d", &x);
+        if (!bucket[x])
+            ++ans, bucket[x] = true;
+    }
+    printf("%d\n", ans);
+    for (int i = 1; i <= 1000; ++i)
+        if (bucket[i])
+            printf("%d ", i);
+        return 0;
+}
