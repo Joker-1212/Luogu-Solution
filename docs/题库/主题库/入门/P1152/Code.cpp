f81078350745bdd7bda89d36333f703ca1803c97
Joker_1212 <joker_1212@163.com>
2022-06-21 20:01:19 +0800

    P1152 C++ Finish

--- docs/题库/主题库/入门/P1152/Code.cpp
+++ docs/题库/主题库/入门/P1152/Code.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 int main()
 {
-    bool a[1005]{0};
-    int n, x, y, cnt;
+    bool a[1005]{0}; // 记录差值是否出现过的数组
+    int n,
+        x, y, // 表示相邻的两个元素
+        cnt;  // 由于需要使用n进行比较，用cnt作为循环控制变量
     scanf("%d%d", &n, &x);
     cnt = n;
     while (--cnt)
     {
         y = x, scanf("%d", &x);
-        if (abs(y - x) > n - 1 || a[abs(y - x)])
-            return (puts("Not jolly"), 0);
+        if (abs(y - x) > n - 1 ||          // 爆range
+            a[abs(y - x)])                 // 差值重复
+            return (puts("Not jolly"), 0); // 直接输出+退出
         a[abs(y - x)] = true;
     }
     puts("Jolly");
