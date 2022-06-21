f0c9ac3128d324e961d72a12e51b40c05f8c1de2
Joker_1212 <joker_1212@163.com>
2022-06-21 11:45:29 +0800

    P1146 C++ Finish

--- docs/题库/主题库/入门/P1146/Code.cpp
+++ docs/题库/主题库/入门/P1146/Code.cpp
@@ -5,15 +5,16 @@ int main()
     int n, a[100]{0};
     scanf("%d", &n);
     printf("%d\n", n);
+    // i 表示当前不翻的硬币
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < n; ++j)
         {
             if (j != i)
-                a[j] = a[j] == 0 ? 1 : 0;
-            printf("%d", a[j]);
+                a[j] ^= 1;      // 因为 a[i] 只有01两种状态，所以与1异或相当于去翻
+            putchar(a[j] ^ 48); // 即a[j] + '0'
         }
-        putchar(10);
+        putchar(10); // putchar('\n')
     }
     return 0;
 }
